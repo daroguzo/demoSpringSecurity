36d74bb5d8b685ad8c229b4353e82ca4b6391347
김진우 <kjw2122@naver.com>
2020-07-17 10:46:58 +0900

    Spring Security : hierarchy role setting

--- src/main/java/me/daroguzo/demo/config/SecurityConfig.java
+++ src/main/java/me/daroguzo/demo/config/SecurityConfig.java
@@ -1,10 +1,12 @@
 package me.daroguzo.demo.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.access.expression.SecurityExpressionHandler;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.access.expression.DefaultWebSecurityExpressionHandler;
 
 @Configuration
 @EnableWebSecurity
@@ -18,14 +20,25 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 //                .withUser("daroguzo").password("{noop}123").roles("USER").and()
 //                .withUser("admin").password("{noop}!@#").roles("ADMIN");
 //    }
+    public SecurityExpressionHandler expressionHandler() {
+        RoleHierarchyImpl roleHierarchy = new RoleHierarchyImpl();
+        roleHierarchy.setHierarchy("ROLE_ADMIN > ROLE_USER");
+
+        DefaultWebSecurityExpressionHandler handler = new DefaultWebSecurityExpressionHandler();
+        handler.setRoleHierarchy(roleHierarchy);
+
+        return handler;
+    }
+
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
                 .mvcMatchers("/", "/info", "/account/**").permitAll()
                 .mvcMatchers("/admin").hasRole("ADMIN")
-                .anyRequest().authenticated();
-
+                .mvcMatchers("/user").hasRole("USER")
+                .anyRequest().authenticated()
+                .expressionHandler(expressionHandler());
         http.formLogin();
         http.httpBasic();
     }
--- src/test/java/me/daroguzo/demo/account/AccountControllerTest.java
+++ src/test/java/me/daroguzo/demo/account/AccountControllerTest.java
@@ -6,11 +6,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.test.context.support.WithAnonymousUser;
-import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
 
-import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestBuilders.formLogin;
+import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.authenticated;
+import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.unauthenticated;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -22,6 +24,9 @@ public class AccountControllerTest {
 
     @Autowired
     MockMvc mockMvc;
+
+    @Autowired
+    AccountService accountService;
 
     @Test
     @WithAnonymousUser
@@ -45,5 +50,46 @@ public class AccountControllerTest {
         mockMvc.perform(get("/admin"))
                 .andDo(print())
                 .andExpect(status().isForbidden());
+    }
+    
+    @Test
+    @Transactional
+    public void login_success() throws Exception {
+        String username = "daroguzo";
+        String password = "123";
+        Account user = createUser(username, password);
+        mockMvc.perform(formLogin().user(user.getUsername()).password(password))
+                .andExpect(authenticated());
+
+    }
+
+    @Test
+    @Transactional
+    public void login_success2() throws Exception {
+        String username = "daroguzo";
+        String password = "123";
+        Account user = createUser(username, password);
+        mockMvc.perform(formLogin().user(user.getUsername()).password(password))
+                .andExpect(authenticated());
+
+    }
+
+    @Test
+    public void login_fail() throws Exception {
+        String username = "daroguzo";
+        String password = "123";
+        Account user = createUser(username, password);
+        mockMvc.perform(formLogin().user(user.getUsername()).password("12345"))
+                .andExpect(unauthenticated()) ;
+
+    }
+
+
+    private Account createUser(String username, String password) {
+        Account account = new Account();
+        account.setUsername(username);
+        account.setPassword(password);
+        account.setRole("USER");
+        return accountService.createNew(account);
     }
 }
