e644c3ac72e48758453b0d3b5e6e7d9b8c9ba92d
김진우 <kjw2122@naver.com>
2020-07-18 21:05:28 +0900

    Spring Security : login/out custom page

--- src/main/java/me/daroguzo/demo/config/SecurityConfig.java
+++ src/main/java/me/daroguzo/demo/config/SecurityConfig.java
@@ -2,13 +2,23 @@ package me.daroguzo.demo.config;
 
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.access.expression.SecurityExpressionHandler;
 import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.security.web.access.expression.DefaultWebSecurityExpressionHandler;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.security.Security;
 
 @Configuration
 @EnableWebSecurity
@@ -44,8 +54,22 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .mvcMatchers("/user").hasRole("USER")
                 .anyRequest().authenticated()
                 .expressionHandler(expressionHandler());
-        http.formLogin();
         http.httpBasic();
+
+        http.exceptionHandling()
+                .accessDeniedHandler(new AccessDeniedHandler() {
+                    @Override
+                    public void handle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, AccessDeniedException e) throws IOException, ServletException {
+                        UserDetails principal = (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+                        String username = principal.getUsername();
+                        System.out.println(username + "is denied to access " + httpServletRequest.getRequestURI());
+                        httpServletResponse.sendRedirect("/access-denied");
+                    }
+                });
+
+        http.formLogin()
+                .loginPage("/login")
+        .permitAll();
 
         http.logout()
             .logoutSuccessUrl("/");
